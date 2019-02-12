0bcd0bf2e7048d0f56d5c2831e7d6f9fbec95e84
Sergii Zlishchev <sergii.zlishchev@gmail.com>
2019-02-12 21:38:26 +0200

    fearture ready

--- test_74.py
+++ test_74.py
@@ -3,13 +3,20 @@ SSN_white_list = [2312, 5436, 9012, 7777]
 while True:
 
     user_ssn_number = input("Enter your SSN number in format 0000: ")
+
     #Length Checker
     if len(user_ssn_number) != 4:
         print ('Invalid SSN, please check the format (0000) !!!')
         continue
+
+    #Digit all checked
     if not user_ssn_number.isdigit():
         print ('Please enter SSN as number')
         continue
+
+    #ssn validation
     if int(user_ssn_number) not in SSN_white_list:
         print ('Invalid SSN number')
         continue
+
+print ("You're welcome")
