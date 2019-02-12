595f21cc0aaf23d542c38645507c56ade3d669f7
Sergii Zlishchev <sergii.zlishchev@gmail.com>
2019-02-12 21:34:13 +0200

    5 ssn number added

--- test_74.py
+++ test_74.py
@@ -1,10 +1,10 @@
-SSN_white_list = [2312, 5436, 9012]
+SSN_white_list = [2312, 5436, 9012, 12345]
 
 while True:
 
     user_ssn_number = input("Enter your SSN number in format 0000: ")
     #Length Checker
-    if len(user_ssn_number) != 4:
+    if len(user_ssn_number) not in  (4, 5):
         print ('Invalid SSN, please check the format (0000) !!!')
         continue
     if not user_ssn_number.isdigit():
