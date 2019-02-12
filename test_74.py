4f0ab98ed2d7c7c000d2bdd1546f6656a5f87072
Sergii Zlishchev <sergii.zlishchev@gmail.com>
2019-02-12 21:25:35 +0200

    SSN validation added

--- test_74.py
+++ test_74.py
@@ -8,4 +8,8 @@ while True:
         print ('Invalid SSN, please check the format (0000) !!!')
         continue
     if not user_ssn_number.isdigit():
-        print
+        print ('Please enter SSN as number')
+        continue
+    if int(user_ssn_number) not in SSN_white_list:
+        print ('Invalid SSN number')
+        continue
