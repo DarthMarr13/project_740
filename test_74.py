17b32ce8d2219f9857b967eac1244e40cb3184ff
Sergii Zlishchev <sergii.zlishchev@gmail.com>
2019-02-12 21:09:23 +0200

    White List Created

---
+++ test_74.py
@@ -0,0 +1,9 @@
+SSN_white_list = [2312, 5436, 9012]
+
+while True:
+
+    user_ssn_number = input("Enter your SSN number in format 0000: ")
+    #Length Checker
+    if len(user_ssn_number) != 4:
+        print ('Invalid SSN, please check the format (0000) !!!')
+        continue
